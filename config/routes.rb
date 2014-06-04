5b09e552b6ddd6df1fe8e1f6a893eb68418fbd92
framgianguyenbui <bui.binh.nguyen@framgia.com>
2014-06-04 09:13:46 +0700

    last try

--- config/routes.rb
+++ config/routes.rb
@@ -12,13 +12,13 @@ Blog::Application.routes.draw do
   resources :comments,  only: [:create, :destroy]
   resources :relationships, only: [:create, :destroy]
   
-  get '/help', to: 'static_pages#help'
-  get '/about', to:'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
+  get '/help' => 'static_pages#help'
+  get '/about' => 'static_pages#about'
+  get '/contact' => 'static_pages#contact'
   
-  get '/users', to: 'users#index'
-  get '/signup', to: 'users#new'
-  get '/signin', to: 'sessions#new'
-  post '/signout', to: 'sessions#destroy', via: :delete
+  get '/users' => 'users#index'
+  get '/signup' =>  'users#new'
+  get '/signin' =>  'sessions#new'
+  post '/signout' =>  'sessions#destroy'
 
 end
